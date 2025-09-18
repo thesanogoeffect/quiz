10167d368baf5ab50a3a6cafeeb32a2690998e03
Jakub Werner <git@jakubwerner.com>
2025-09-18 20:56:33 +0200

    disabled devtools

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ import { Title } from "#build/components";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-09-18",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: true,
   modules: ["@pinia/nuxt", "vuetify-nuxt-module"],
   vuetify: {
