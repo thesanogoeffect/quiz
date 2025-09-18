89f14b7b92f592c068ceb0b61fa3f789e9b3569e
Jakub Werner <git@jakubwerner.com>
2025-09-18 21:14:30 +0200

    added a base url

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,9 @@ import { Title } from "#build/components";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-09-18",
+  app: {
+    baseURL: '/ipt/'
+  },
   devtools: { enabled: false },
   ssr: true,
   modules: ["@pinia/nuxt", "vuetify-nuxt-module"],
